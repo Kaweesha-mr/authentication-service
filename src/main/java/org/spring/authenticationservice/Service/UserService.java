8691e780366b622b6cee705371eca90ae180615c
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-12-18 21:44:28 +0530

    updated User service and repo

--- src/main/java/org/spring/authenticationservice/Service/UserService.java
+++ src/main/java/org/spring/authenticationservice/Service/UserService.java
@@ -1,13 +1,29 @@
 package org.spring.authenticationservice.Service;
 
+import org.spring.authenticationservice.model.User;
 import org.spring.authenticationservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
-    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public void saveUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+
+        userRepository.save(user);
+    }
+
+    public Boolean findUserByUsername(String email){
+        return userRepository.existsByEmail(email);
+    }
+
+
 }
