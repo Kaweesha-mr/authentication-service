314a22e89c02681bf384afba5034e475bca5f47f
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-12-18 21:44:44 +0530

    authentication manger added

--- src/main/java/org/spring/authenticationservice/config/SecurityConfig.java
+++ src/main/java/org/spring/authenticationservice/config/SecurityConfig.java
@@ -3,10 +3,13 @@ package org.spring.authenticationservice.config;
 import org.spring.authenticationservice.Service.UserDetailServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -38,10 +41,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/req").permitAll()  // Allow public access to login and req
+                        .requestMatchers("/login", "/register").permitAll()  // Allow public access to login and req
                         .anyRequest().authenticated()  // All other requests require authentication
                 )
-                .authenticationProvider(authenticationProvider());  // Register authentication provider
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));  // Register authentication provider
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
