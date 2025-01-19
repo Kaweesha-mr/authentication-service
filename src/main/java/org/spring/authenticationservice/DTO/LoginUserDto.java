d90ad0e945882a6d806c38fda54df6dcabe6bc02
kaweesha-mr <kaweesha.mr@gmail.com>
2025-01-19 11:57:15 +0530

    chore: test added with chatgpt

--- src/main/java/org/spring/authenticationservice/DTO/LoginUserDto.java
+++ src/main/java/org/spring/authenticationservice/DTO/LoginUserDto.java
@@ -8,6 +8,11 @@ public class LoginUserDto {
     private String password;
     private List<String> roles = new ArrayList<>();
 
+    public LoginUserDto(String mail, String password) {
+        this.email = mail;
+        this.password = password;
+    }
+
     public List<String> getRoles() {
         return roles;
     }
